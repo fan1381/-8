8fa46afbc40c9b78bfda8d4570e0a8496bfec4ae
fan1381 <57989095+fan1381@users.noreply.github.com>
2019-12-19 11:35:52 +0800

    登录功能完成

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,58 +1,99 @@
 <template>
   <div class="login">
-      <!-- 卡片 -->
-<el-card class="box-card">
-    <div class="title">
-        <img src="../../assets/img/logo_index.png" alt="">
-    </div>
-    <el-form>
-    <el-form-item>
-        <el-input v-model="input" placeholder="请输入手机号"></el-input>
-    </el-form-item>
-     <el-form-item>
-        <el-input style="width:60%" v-model="input" placeholder="请输入验证码"></el-input>
-     <el-button style="float:right">发送验证码</el-button>
-    </el-form-item>
+    <!-- 卡片 -->
+    <el-card class="box-card">
+      <div class="title">
+        <img src="../../assets/img/logo_index.png" alt />
+      </div>
+      <el-form :model="loginForm" :rules="loginRules" ref="myForm">
 
-    <el-form-item>
- <el-checkbox v-model="checked">我已阅读并同意用户协议和隐私条款</el-checkbox>
-    </el-form-item>
+        <el-form-item prop="mobile">
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
 
-<el-form-item>
- <el-button style="width:100%" type="primary">登 录</el-button>
+        <el-form-item prop="code">
+          <el-input style="width:60%" v-model="loginForm.code" placeholder="请输入验证码"></el-input>
+          <el-button style="float:right">发送验证码</el-button>
+        </el-form-item>
 
-</el-form-item>
-</el-form>
-</el-card>
-<!-- 表单 -->
+        <el-form-item prop="check">
+          <el-checkbox v-model="loginForm.check">我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
 
+        <el-form-item>
+          <el-button style="width:100%" type="primary" @click="login_submit">登 录</el-button>
+        </el-form-item>
+
+      </el-form>
+    </el-card>
+    <!-- 表单 -->
   </div>
 </template>
 
 <script>
-export default {
 
+export default {
+  data () {
+    return {
+      loginForm: {
+        mobile: '', // 手机号
+        code: '', // 验证码
+        check: false // 是否被勾选
+      },
+      loginRules: {
+        mobile: [
+          { required: true, message: '请输入手机号' },
+          { pattern: /^1[3456789]\d{9}$/, message: '手机号格式不正确' }
+        ],
+        code: [
+          { required: true, message: '请输入验证码' },
+          { pattern: /^\d{6}$/, message: '请输入正确的验证码' }
+        ],
+        check: [
+          {
+            validator: function (rule, value, callback) {
+              if (value) {
+                callback()
+              } else {
+                callback(new Error('您还没有勾选协议哦'))
+              }
+            }
+          }
+        ]
+      }
+
+    }
+  },
+  methods: {
+    login_submit () {
+      this.$refs.myForm.validate(function (isok) {
+        if (isok) {
+          console.log('登录成功')
+        }
+      })
+    }
+  }
 }
 </script>
 
 <style lang='less' scoped>
-.login{
-    background-image: url('../../assets/img/logo.png');
-    height: 100vh;
-    background-size: cover;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-.box-card{
+.login {
+  background-image: url("../../assets/img/logo.png");
+  height: 100vh;
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .box-card {
     width: 440px;
     height: 400px;
-    .title{
+    .title {
       text-align: center;
       margin-bottom: 20px;
-      img{
+      img {
         height: 45px;
       }
     }
-}
+  }
 }
 </style>
