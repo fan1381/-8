9864672bd4b004a1a849c4aaf5289d8604efc9be
fan1381 <57989095+fan1381@users.noreply.github.com>
2019-12-20 16:37:44 +0800

    home页头部结束

---
+++ src/components/home/home-header.vue
@@ -0,0 +1,47 @@
+<template>
+  <el-row class="box" type="flex" align="middle">
+    <el-col :span="12" class="left">
+      <i class="el-icon-s-fold"></i>
+      <span>江苏传智播客教育科技股份有限公司</span>
+    </el-col>
+    <el-col :span="12">
+      <el-row class="right" type="flex" justify="end" align="middle">
+        <img src="../../assets/img/home.jpeg" alt />
+
+        <el-dropdown class="name">
+          <span>光头强</span>
+          <el-dropdown-menu>
+            <el-dropdown-item>个人信息</el-dropdown-item>
+            <el-dropdown-item>Git地址</el-dropdown-item>
+            <el-dropdown-item>退出</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </el-row>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang='less' scoped>
+.box {
+  height: 60px;
+  .left {
+    font-size: 20px;
+    span {
+      font-size: 18px;
+    }
+  }
+  .right {
+      line-height: 60px;
+    img {
+      width: 40px;
+      height: 40px;
+      border-radius: 50%;
+      margin-right: 6px
+    }
+  }
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,25 +1,31 @@
-<template>
 
-<el-container>
-    <el-aside>
-        <home-left style='height:100vh;background-color:#353b4e'></home-left>
+// home页根组件
+<template>
+  <el-container>
+    <el-aside style="width:230px">
+      <home-left style="height:100vh;background-color:#353b4e"></home-left>
     </el-aside>
-<el-container>
-    <el-header>header</el-header>
-    <el-main>zhuyao</el-main>
-</el-container>
-</el-container>
+    <el-container>
+      <el-header>
+        <home-header>
+
+        </home-header>
+      </el-header>
+      <el-main>zhuyao</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 import homeLeft from '../../components/home/home-left'
+import homeHeader from '../../components/home/home-header'
 export default {
   components: {
-    'home-left': homeLeft
+    'home-left': homeLeft,
+    'home-header': homeHeader
   }
 }
 </script>
 
 <style>
-
 </style>
