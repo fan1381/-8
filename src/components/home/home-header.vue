0aeb2fa360651ac5cd73545ed20c9210b81c2670
fan1381 <57989095+fan1381@users.noreply.github.com>
2019-12-21 09:51:56 +0800

    home页退出功能及左侧路径跳转

--- src/components/home/home-header.vue
+++ src/components/home/home-header.vue
@@ -6,14 +6,15 @@
     </el-col>
     <el-col :span="12">
       <el-row class="right" type="flex" justify="end" align="middle">
+        <!-- 用三元表达式满足没有头像时显示默认图片，由于不能写固定地址，需要将路径转成变量 -->
         <img :src="userInfo.photo ? userInfo.photo:img" alt />
 
-        <el-dropdown class="name">
+        <el-dropdown class="name" @command='click'>
           <span>{{ userInfo.name }}</span>
           <el-dropdown-menu>
-            <el-dropdown-item>个人信息</el-dropdown-item>
-            <el-dropdown-item>Git地址</el-dropdown-item>
-            <el-dropdown-item>退出</el-dropdown-item>
+            <el-dropdown-item command='info'>个人信息</el-dropdown-item>
+            <el-dropdown-item command='git'>Git地址</el-dropdown-item>
+            <el-dropdown-item command='quit'>退出</el-dropdown-item>
           </el-dropdown-menu>
         </el-dropdown>
       </el-row>
@@ -39,6 +40,19 @@ export default {
     }).then(res => {
       this.userInfo = res.data.data
     })
+  },
+  methods: {
+    click (command) {
+      this.$message('触发了' + command)
+      if (command === 'info') {
+
+      } else if (command === 'git') {
+        window.location.href = 'https://github.com/fan1381/-8'
+      } else {
+        window.localStorage.removeItem('user-token')
+        this.$router.push('/login')
+      }
+    }
   }
 
 }
--- src/components/home/home-left.vue
+++ src/components/home/home-left.vue
@@ -3,18 +3,18 @@
      <div class="img">
             <img src="../../assets/img/logo_admin.png" alt="">
       </div>
-      <el-menu background-color='#353b4e' text-color='#adafb5' active-text-color="#ffd04b">
+      <el-menu router="true" background-color='#353b4e' text-color='#adafb5' active-text-color="#ffd04b">
          <el-menu-item>首页</el-menu-item>
          <!-- 内容管理 -->
-         <el-submenu>
+         <el-submenu index='1'>
              <template slot="title">内容管理</template>
-             <el-menu-item>发布文章</el-menu-item>
-             <el-menu-item>文章列表</el-menu-item>
-             <el-menu-item>评论管理</el-menu-item>
-             <el-menu-item>素材管理</el-menu-item>
+             <el-menu-item index="/home/publish">发布文章</el-menu-item>
+             <el-menu-item index="/home/articles">文章列表</el-menu-item>
+             <el-menu-item index="/home/comment">评论管理</el-menu-item>
+             <el-menu-item index="/home/material">素材管理</el-menu-item>
          </el-submenu>
         <!-- 粉丝管理 -->
-         <el-submenu>
+         <el-submenu index='2'>
              <template slot="title">粉丝管理</template>
              <el-menu-item>图文数据</el-menu-item>
              <el-menu-item>粉丝概况</el-menu-item>
