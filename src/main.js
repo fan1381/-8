a64cc914a7219e2ed768b60a378a1ad9aa2d7c11
fan1381 <57989095+fan1381@users.noreply.github.com>
2019-12-21 21:23:26 +0800

    导航守卫，没有token进不去home页

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './permission/index'
 import router from './router'
 import ElementUI from 'element-ui' // 引入UI
 import Component from './components/index'
