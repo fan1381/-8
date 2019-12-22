91e3719677e03c481b87fe557dd1bd92177e003f
fan1381 <57989095+fan1381@users.noreply.github.com>
2019-12-22 12:00:08 +0800

    评论管理，显示列表

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -3,18 +3,37 @@
     <crumb slot="header">
       <template slot="title">评论管理</template>
     </crumb>
-    <el-table>
-        <el-table-column width="600" label="标题"></el-table-column>
-        <el-table-column label="评论状态"></el-table-column>
-        <el-table-column label="总评论数"></el-table-column>
-        <el-table-column label="粉丝评论数"></el-table-column>
+    <el-table :data="list">
+        <el-table-column prop="title" width="600" label="标题"></el-table-column>
+        <el-table-column prop="comment_status" label="评论状态"></el-table-column>
+        <el-table-column prop="total_comment_count" label="总评论数"></el-table-column>
+        <el-table-column prop="fans_comment_count" label="粉丝评论数"></el-table-column>
         <el-table-column label="操作"></el-table-column>
     </el-table>
   </el-card>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      list: []
+    }
+  },
+  methods: {
+    getComment () {
+      this.$axios({
+        url: '/articles',
+        params: { response_type: 'comment' }
+      }).then(result => {
+        this.list = result.data.results
+      })
+    }
+  },
+  created () {
+    this.getComment()
+  }
+}
 </script>
 
 <style>
